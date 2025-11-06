c12eab8c808cfdb51526402bfa0dde516a1cca6a
hishamrehab <hishamrehab30@gmail.com>
2025-11-07 01:08:54 +0200

    Reducers

--- redux/script.js
+++ redux/script.js
@@ -4,9 +4,27 @@ const ORDER_PIZZA = "ORDER_PIZZA"
 const action = 
 
 // Action Creator
+
 function orderPizza() {
     return {
     type : ORDER_PIZZA,
     shop_name : "Pizza Shop"
 }
+}
+
+// Reducer
+const initialState = {
+    pizzaBase : 100,
+
+}
+
+const reducer = (state = initialState , action) => {
+  switch(action.type) {
+    case ORDER_PIZZA: 
+    return {
+      pizzaBase: pizzaBase - 1
+    }
+    default : 
+    return state;
+  }
 }
