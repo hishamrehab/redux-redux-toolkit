f7ee1a6e3bd58ff219059c776ab559c0d1df5b88
hishamrehab <hishamrehab30@gmail.com>
2025-11-07 00:45:36 +0200

    action and action creator

--- redux/script.js
+++ redux/script.js
@@ -1,1 +1,12 @@
-console.log("Welcome To Redux");
+const ORDER_PIZZA = "ORDER_PIZZA"
+
+// Action
+const action = 
+
+// Action Creator
+function orderPizza() {
+    return {
+    type : ORDER_PIZZA,
+    shop_name : "Pizza Shop"
+}
+}
