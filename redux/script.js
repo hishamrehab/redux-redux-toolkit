b81c5885a8e848ea75e6b6088bb7c0a4e2cc8bb0
hishamrehab <hishamrehab30@gmail.com>
2025-11-07 02:27:19 +0200

    Store Implementation

--- redux/script.js
+++ redux/script.js
@@ -1,30 +1,56 @@
+const redux = require("redux");
+const createStore = redux.createStore;
 const ORDER_PIZZA = "ORDER_PIZZA"
 
 // Action
-const action = 
+
+// const action = {
+//     type : ORDER_PIZZA,
+//     shop_name : "Pizza Shop"
+// }
+
 
 // Action Creator
 
 function orderPizza() {
     return {
     type : ORDER_PIZZA,
-    shop_name : "Pizza Shop"
-}
+    
+    }
 }
 
 // Reducer
 const initialState = {
     pizzaBase : 100,
-
 }
 
 const reducer = (state = initialState , action) => {
   switch(action.type) {
     case ORDER_PIZZA: 
     return {
-      pizzaBase: pizzaBase - 1
+      ...state,
+      pizzaBase: state.pizzaBase - 1
     }
-    default : 
+    default: 
     return state;
   }
 }
+
+
+// STORE 
+// 1 - Store needs to hold application  state.
+const store = createStore(reducer);
+
+// 2 - It exposes a method called getState which gives your
+// application access to the current state in the store
+console.log("Initial State" , store.getState());
+
+// 3- Register listeners via subscribe(listener)
+const unsubscripe = store.subscribe(() => console.log("Updated State" , store.getState()));
+
+// 4 -Allows state to be updated via dispatch(action)
+store.dispatch(orderPizza());
+store.dispatch(orderPizza());
+store.dispatch(orderPizza());
+store.dispatch(orderPizza());
+unsubscripe();
