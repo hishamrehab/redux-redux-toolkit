cd9cace6aebe7ea1f0e3b9efdfcab99ab08a5e01
hishamrehab <hishamrehab30@gmail.com>
2025-11-11 01:29:24 +0200

    useSelector and useDispatch

--- 2-react-redux/src/App.jsx
+++ 2-react-redux/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css"
-import PizzaBox from "./components/PizzaBox.jsx"
+// import PizzaBox from "./components/PizzaBox.jsx"
 import { Provider } from "react-redux"
 import store from "./components/redux/store.js"
+import HooksContainer from "./components/HooksContainer.jsx"
 
 function App() {
   return (
     <Provider  store={store}>
-      <PizzaBox />
+      {/* <PizzaBox /> */}
+   <HooksContainer />
     </Provider>
   )
 }
