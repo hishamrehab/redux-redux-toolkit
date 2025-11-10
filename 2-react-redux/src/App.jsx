2b55c83f0d5c9f64d87de0db3f40c28a635dd1b9
hishamrehab <hishamrehab30@gmail.com>
2025-11-10 23:59:41 +0200

    react-redux

---
+++ 2-react-redux/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import PizzaBox from "./components/PizzaBox.jsx"
+
+function App() {
+ 
+
+  return (
+  <PizzaBox />
+  )
+}
+
+export default App
---
+++ 2-react-redux/src/components/PizzaBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PizzaBox = () => {
+  return (
+    <div className="container">
+        <h2 className='text'>Number of Pizza Base available - 100</h2>
+       <button className='btn'>Order Pizza</button>  
+    </div>
+  )
+}
+
+export default PizzaBox
